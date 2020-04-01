318b898f8cf69e389fca48be018e8ac792793862
arcum-omni <travis.l.eiler@gmail.com>
2020-04-01 16:46:15 -0700

    Added Migration

---
+++ Migrations/20200401233651_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MealPlanner.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calendars",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subject = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 300, nullable: true),
+                    Start = table.Column<DateTime>(nullable: false),
+                    End = table.Column<DateTime>(nullable: true),
+                    ThemeColor = table.Column<string>(maxLength: 10, nullable: true),
+                    IsFullDay = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calendars", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calendars");
+        }
+    }
+}
