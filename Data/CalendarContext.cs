642c8e1943f3208431fe6ed0f3d39428d99bd8d3
arcum-omni <travis.l.eiler@gmail.com>
2020-04-01 16:17:11 -0700

    Created DbContext

---
+++ Data/CalendarContext.cs
@@ -0,0 +1,20 @@
+﻿using MealPlanner.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Data
+{
+    public class CalendarContext : DbContext
+    {
+        public CalendarContext(DbContextOptions<CalendarContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Calendar> Calendars { get; set; }
+    }
+
+
+}
---
+++ Models/Calendar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MealPlanner.Models
+{
+    // http://www.dotnetawesome.com/2017/06/event-calendar-in-aspnet-mvc.html
+
+    public class Calendar
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? End { get; set; }
+
+        [StringLength(10)]
+        public string ThemeColor { get; set; }
+
+        public bool IsFullDay { get; set; }
+    }
+}
